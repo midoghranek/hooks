96870f82fa72f7ef20da5f152827837621177811
midoghranek <midoghranek@hotmail.com>
2021-03-05 00:30:54 +0200

    🐛 FIX: window undefined

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -3,12 +3,7 @@ import { useDidUpdate } from "./useDidUpdate";
 import { useWillUnmount } from "./useWillUnmount";
 
 export const useMediaQuery = (mediaQuery: string) => {
-  const isMedia = () => {
-    if (typeof window === "object") return "matchMedia" in window;
-    return false;
-  };
-
-  if (isMedia) {
+  if (typeof window === "object" || "matchMedia" in window) {
     const [match, setMatch] = useState(!!window.matchMedia(mediaQuery).matches);
 
     const mediaQueryList = window?.matchMedia(mediaQuery);
