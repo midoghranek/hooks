e050bce95fe7516992c471f4a20a6de5477bb3ff
midoghranek <midoghranek@hotmail.com>
2021-03-04 15:37:33 +0200

    👌 IMPROVEMENTS

--- src/hooks/useDidUpdate.ts
+++ src/hooks/useDidUpdate.ts
@@ -1,7 +1,7 @@
 // useDidUpdate similar to componentDidUpdate
 import { useEffect, useRef } from "react";
 
-export const useDidUpdate = (callback: Function, deps: []) => {
+export const useDidUpdate = (callback: Function, deps: any[]) => {
   const didMountRef = useRef(false);
   useEffect(
     () => (didMountRef.current ? callback?.() : (didMountRef.current = true)),
